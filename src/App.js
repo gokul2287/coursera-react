33d41540ceff27d5228042b29020b79489c6f8c4
gokul2287 <gokulagiwal2287@gmail.com>
2020-10-10 23:38:09 +0100

    'Redux'

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import React from 'react';
 import Main from './components/MainComponent';
 import './App.css'; 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-      {/*  <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>  */}
-        <Main /> 
-      </div>
-    </BrowserRouter>
+    <Provider store = {store}>
+      <BrowserRouter>
+        <div>
+        {/*  <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>  */}
+          <Main /> 
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 export default App;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,22 +4,30 @@ import Menu from './MenuComponent';
 import DishDetails from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
+
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import { connect, useSelector } from 'react-redux';
+
+// const mapStateToProps = state => {
+//   return {
+//     dishes: state.dishes,
+//     comments: state.comments,
+//     promotions: state.promotions,
+//     leaders: state.leaders
+//   }
+// }
 
 function Main() {
-
+  
+  const dd = useSelector(state =>state)
   //Defining state 
-  const [dishes, setDishes] = useState(DISHES);
-  const [comments, setComments] = useState(COMMENTS);
-  const [promotions] = useState(PROMOTIONS);
-  const [leaders] = useState(LEADERS);
+  // const [dishes, setDishes] = useState(DISHES);
+  // const [comments, setComments] = useState(COMMENTS);
+  // const [promotions] = useState(PROMOTIONS);
+  // const [leaders] = useState(LEADERS);
   //const [selectedDish, setselectedDish] = useState(null);
   // console.log("selectedDish",selectedDish);
 
@@ -30,16 +38,16 @@ function Main() {
 
   const HomePage = () => {
     return (
-      <Home dish = {dishes.filter((dish) => dish.featured)[0]}
-      promotion = {promotions.filter((promo) => promo.featured)[0]}
-      leader = {leaders.filter((lead)=> lead.featured)[0]}></Home>
+      <Home dish = {dd.dishes.filter((dish) => dish.featured)[0]}
+      promotion = {dd.promotions.filter((promo) => promo.featured)[0]}
+      leader = {dd.leaders.filter((lead)=> lead.featured)[0]}></Home>
     );
   } 
 
   const DishWithID = ({match}) => {
     return (
-      <DishDetails dish = {dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-      comments = {comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}>
+      <DishDetails dish = {dd.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+      comments = {dd.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}>
       </DishDetails>
     );
   }
@@ -49,10 +57,10 @@ function Main() {
       <Header></Header>
       <Switch>
         <Route path ="/home" component = {HomePage}></Route>
-        <Route exact path = "/menu" component = {()=> < Menu dishes = {dishes} />}></Route>
+        <Route exact path = "/menu" component = {()=> < Menu dishes = {dd.dishes} />}></Route>
         <Route path = "/menu/:dishId" component = { DishWithID }></Route>
         <Route exact path = "/contactus" component = { Contact }></Route>
-        <Route exact path = "/aboutus" component = {() => < About leaders = {leaders} />}></Route>
+        <Route exact path = "/aboutus" component = {() => < About leaders = {dd.leaders} />}></Route>
         <Redirect to = "/home"></Redirect>
       </Switch>
       {/* <Menu dishes = {dishes} onClick = {(dishId) => setselectedDish(dishId)}/>  */}
